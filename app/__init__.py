0a2e56ef33f3ffd7ccaabb3e3d49137ff7353239
c3lphie <c3lphie@protonmail.com>
2022-10-02 13:53:24 +0200

    Oauth and add event implemented

--- app/__init__.py
+++ app/__init__.py
@@ -8,21 +8,30 @@ from flask import (
 )
 from flask_sqlalchemy import SQLAlchemy
 
+import requests as r
+
+from pprint import pprint
+
 from werkzeug.security import check_password_hash, generate_password_hash
 
 from os import environ
 
 
-
 app = Flask(__name__)
 
+app.config["GUILD_ID"] = environ["DISCORD_GUILD_ID"]
+app.config["CLIENT_ID"] = environ["DISCORD_CLIENT_ID"]
+app.config["CLIENT_SECRET"] = environ["DISCORD_CLIENT_SECRET"]
+app.config["ADMIN_ROLE_ID"] = environ["DISCORD_ADMIN_ROLE_ID"]
+app.config["REDIRECT_URI"] = environ["DISCORD_REDIRECT_URI"]
 try:
-    app.secret_key = environ['APP_SECRET_KEY']
+    app.secret_key = environ["APP_SECRET_KEY"]
 except KeyError as e:
     print("Caught KeyError, 'APP_SECRET_KEY' not set.\nUsing default")
     app.secret_key = "super Strong and Secret Key"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///jutlandia.db"
+
 db = SQLAlchemy(app)
 
 class Admins(db.Model):
@@ -49,6 +58,15 @@ class Events(db.Model):
         self.location = location
         self.over     = over
     
+def session_filter(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if "token" not in session:
+            return redirect(url_for("oauth_get_token"))
+        return func(*args, **kwargs)
+
+    return wrapper
+
 
 @app.route("/", methods=["GET"])
 def index():
@@ -63,15 +81,47 @@ def index():
     return render_template("index.html",
                            upcoming_events=upcoming,
                            finished_events=finished)
+def get_discord_roles():
+    guild_id = app.config["GUILD_ID"]
+    api_endpoint = f"/users/@me/guilds/{guild_id}/member"
+    headers = {"Authorization": f"Bearer {session['token']}"}
+    result = r.get(
+        f"http://discord.com/api/{api_endpoint}", headers=headers
+    )
+    print(result.status_code)
+    print(result.text)
+    result = result.json()
+    pprint(result)
+    return result["roles"]
 
 @app.route("/admin", methods=["GET"])
+@session_filter
 def admin():
-   if "username" in session:
-       return render_template(
-           "admin.html",
-       )
+    discord_roles = get_discord_roles()
+    admin_role = app.config["ADMIN_ROLE_ID"]
+    if admin_role in discord_roles:
+        all_events = Events.query.order_by(Events.id).all()
+        pprint(all_events)
+        return render_template(
+            "admin.html",
+            events=all_events
+        )
+    else:
+        return "not admin stay away!!!"
 
-   return redirect(url_for('login'))
+
+@app.route("/api/add_event", methods=["POST"])
+@session_filter
+def add_event():
+    name = request.form["name"]
+    link = request.form["link"]
+    date = request.form["date"]
+    location = request.form["location"]
+
+    event = Events(name, date, link, location)
+    db.session.add(event)
+    db.session.commit()
+    return "ok"
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -93,3 +143,36 @@ def login():
             return render_template("login.html")
         else:
             return redirect(url_for('admin'))
+@app.route("/oauth")
+def oauth_get_token():
+    redirect_uri = app.config["REDIRECT_URI"]
+    client_id = app.config["CLIENT_ID"]
+    client_secret = app.config["CLIENT_SECRET"]
+    code = request.args.get("code", "")
+
+    if "token" in session:
+        return redirect(url_for("admin"))
+
+    if not code:
+        return redirect(
+            "https://discord.com/oauth2/authorize?client_id="
+            + client_id
+            + "&redirect_uri="
+            + redirect_uri
+            + "&response_type=code&scope=guilds.members.read"
+        )
+    else:
+        code = request.args["code"]
+        data = {
+            "client_id": client_id,
+            "client_secret": client_secret,
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": redirect_uri,
+        }
+        headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        result = r.post(
+            "https://discord.com/api/oauth2/token", data=data, headers=headers
+        ).json()
+        session["token"] = result["access_token"]
+        return redirect(url_for("oauth_get_token"))
