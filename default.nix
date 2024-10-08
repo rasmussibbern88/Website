ac5f2e24a4f2d157d0b0c255a4d67af8dde151fd
c3lphie <c3lphie@protonmail.com>
2024-10-08 16:50:59 +0200

    pin nixpkgs and ignore python conflicts

--- default.nix
+++ default.nix
@@ -1,5 +1,9 @@
-{ pkgs ? import <nixpkgs> {}}:
+{ nixpkgs ? (builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/ba02fd0434ed92b7335f17c97af689b9db1413e0.tar.gz";
+    sha256 = "1pjx78qb3k4cjkbwiw9v0wd545h48fj4criazijwds53l0q4dzn1";
+})}:
 let
+  pkgs = import nixpkgs {};
   pythonpkgs = pkgs.python310.withPackages (p: with p; [
     flask
     flask_sqlalchemy
@@ -17,5 +21,7 @@ buildPythonPackage {
   nativeBuildInputs = [ setuptools-scm ];
 
   doCheck = false;
+
+  pythonCatchConflictsPhase = "";
   #pythonImportsCheck = [ "jutlandia_site"];
 }
